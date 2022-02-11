fc360f8fe439223f7ef75165fd0a3adb972e9721
ZaCorder <99446830+ZaCorder@users.noreply.github.com>
2022-02-10 19:51:13 -0500

    Version submitted for grading

---
+++ PnP Assistant/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult DiceRoller()
+        {
+            ViewBag.Message = "Dice Roller";
+
+            return View();
+        }
+
+        public ActionResult Characters()
+        {
+            ViewBag.Message = "Characters";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact";
+
+            return View();
+        }
+        public ActionResult CharacterSheetViewMockUp()
+        {
+            ViewBag.Message = "Character Sheet";
+
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            ViewBag.Message = "Log In";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register";
+
+            return View();
+        }
+    }
+}
